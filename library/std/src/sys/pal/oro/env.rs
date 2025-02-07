0a1316f7f0b89d7e120b4740df163d190f1bbe23
Josh Junon <junon@oro.sh>
2025-02-07 21:21:24 +0100

    update pal env values for Oro

--- library/std/src/sys/pal/oro/env.rs
+++ library/std/src/sys/pal/oro/env.rs
@@ -1,9 +1,9 @@
 pub mod os {
     pub const FAMILY: &str = "";
-    pub const OS: &str = "";
+    pub const OS: &str = "oro";
     pub const DLL_PREFIX: &str = "";
     pub const DLL_SUFFIX: &str = "";
     pub const DLL_EXTENSION: &str = "";
-    pub const EXE_SUFFIX: &str = "";
-    pub const EXE_EXTENSION: &str = "";
+    pub const EXE_SUFFIX: &str = ".oro";
+    pub const EXE_EXTENSION: &str = "oro";
 }
